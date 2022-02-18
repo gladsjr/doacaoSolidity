3dfd5511aea623fad59a05955740934fe29d90a7
Gladstone Moises Arantes Junior <glads@bndes.gov.br>
2022-02-18 11:08:42 -0300

    Update doacao.sol

--- doacao.sol
+++ doacao.sol
@@ -17,51 +17,57 @@ interface IERC20 {
 }
 
 contract Doacao {
-   IERC20 public tokenMPE1;
+    IERC20 public tokenMPE1;
 
-   IERC20 public tokenMPE2;
+    IERC20 public tokenMPE2;
 
-  address public owner;
-    constructor(address ower1,IERC20 MPE1,IERC20 MPE2) public {
-     owner = ower1;
-     tokenMPE1 = MPE1;
-     tokenMPE2 = MPE2;
-   }
-
-  function transferMPE1(address  recepient, uint256 value) public{
-    if(msg.sender== owner){
-    tokenMPE1.transfer(recepient,value);
-    }
-  }
-  function transferMPE2(address  recepient, uint256 value) public{
-    if(msg.sender== owner){
-    tokenMPE2.transfer(recepient,value);
-    }
-  }
-  fallback() external payable {
-      
-  }
+    address public owner;
   
-  function sendEthRMoney(address payable recepient, uint256 value,uint256 cent) external{
-    if(msg.sender== owner){
-    uint eth = (cent *1000000000000000);
+    constructor(address ower1,IERC20 MPE1,IERC20 MPE2) public {
+        owner = ower1;
+        tokenMPE1 = MPE1;
+        tokenMPE2 = MPE2;
+    }
+
+    function donateMPE1(address  recepient, uint256 value) public {
+        if (msg.sender== owner) {
+            tokenMPE1.transfer(recepient,value);
+        }
+    }
+  
+    function donateMPE2 (address  recepient, uint256 value) public {
+        if (msg.sender== owner) {
+            tokenMPE2.transfer(recepient,value);
+        }
+    }
+  
+    fallback() external payable {
+      
+    }
+  
+    fuction convertCents (uint256 value, uint256 cents, uint decimals) internal view returns (uint256) {
+        uint256 finalValue =         
+    }
+  
+  function donateEther (address payable recepient, uint256 value, uint256 cent) external {
+    require(msg.sender == owner);
+    
+    uint eth = (cent * 1000000000000000);
     eth = (1 ether * value) + eth;
     recepient.transfer( eth );
-    }
   }
   
-  function sendETHR(address payable recepient) external {
-    recepient.transfer(1 ether );
-  }
-  function getBalanceEth() external view returns(uint256){
-      return address(this).balance;
-  }
-  function getBalanceMPE1() external view returns(uint256){
+    function getBalanceEth() external view returns(uint256){
+        return address(this).balance;
+    }
+
+    function getBalanceMPE1() external view returns(uint256){
       address conta = address(this);
       return tokenMPE1.balanceOf(conta);
-  }
-  function getBalanceMPE2() external view returns(uint256){
+    }
+ 
+    function getBalanceMPE2() external view returns(uint256){
       address conta = address(this);
       return tokenMPE2.balanceOf(conta);
-  }
+    }
 }
